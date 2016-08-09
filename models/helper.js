0e75020b268da14c29b9ac6369025bc881bdbfc3
Masanori Ohgita <mp@ohgita.info>
2016-08-09 15:46:15 +0900

    Change: Improved document

--- models/helper.js
+++ models/helper.js
@@ -1,27 +1,51 @@
-module.exports = {
+'use strict';
 
 
-	removeSpaces: function (str) {
+/**
+ * Helper static class
+ * @namespace
+ */
+var Helper = function () {
 
-		return str.replace(new RegExp(/(\s|&nbsp;)/g), '');
-
-	},
-
-
-	convertTimeStrToSeconds: function (str) {
-
-		if (str.match(/(\d+)時間(\d+)分/)) {
-
-			var h = RegExp.$1;
-			var m = RegExp.$2;
-
-			return (h * 60 * 60) + (m * 60);
-
-		}
-
-		return 0;
-
-	}
-	
+	throw 'This class can only be used as static class.';
 
 };
+
+
+/**
+ * Remove space characters
+ * @param  {String} str Source string
+ * @return {String}     Processed string
+ */
+Helper.removeSpaces = function (str) {
+
+	return str.replace(new RegExp(/(\s|&nbsp;)/g), '');
+
+};
+
+
+/**
+ * Convert the time string to seconds
+ * @param {String} str  Source string
+ * @param {Number} seconds
+ */
+Helper.convertTimeStrToSeconds = function (str) {
+
+	if (str.match(/(\d+)時間(\d+)分/)) {
+
+		var h = RegExp.$1;
+		var m = RegExp.$2;
+
+		return (h * 60 * 60) + (m * 60);
+
+	}
+
+	return 0;
+
+};
+
+
+// ----
+
+
+module.exports = Helper;
